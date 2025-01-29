d52f4bc8dd63b88a5feee473ef2c85fcae073544
Evan Latham <evan.latham@kellogg.northwestern.edu>
2025-01-29 21:14:26 +0000

    updated

--- school/school.sql
+++ school/school.sql
@@ -16,4 +16,31 @@ CREATE TABLE students (
 
 -- Create the rest of the tables
 
+CREATE TABLE teachers (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  bio TEXT
 
+);
+
+CREATE TABLE courses (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  description TEXT
+);
+
+CREATE TABLE sections (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  time TEXT,
+  course_id INTEGER,
+  teacher_id INTEGER
+);
+
+--- we want the student to be able to enroll in a course. So enrollments table should have student name, course name,
+CREATE TABLE enrollments (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  student_id INTEGER,
+  section_id INTEGER,
+  
+);
